fad9ace9865429fa95da4b398c8467e15ed0d49e
AndresParraSilva <parra.andres@gmail.com>
2024-06-18 06:17:58 -0300

    remove_accents() y sonidos

--- ahorcado.py
+++ ahorcado.py
@@ -12,32 +12,34 @@ def reload():
 
 def button(letter):
     if st.button(letter, use_container_width=True, disabled=state.finished or letter in state.known_letters or letter in state.attempted_letters):
-        if letter in unidecode(state.answer):
+        if letter in remove_accents(state.answer):
             state.known_letters.add(letter)
         else:
             state.attempted_letters.add(letter)
 
 
+def remove_accents(encoded):
+    """Removes the accent marks, but not for Ññ"""
+    decoded = "".join([c if c in "Ññ" else unidecode(c) for c in encoded])
+    return decoded
+
+
 st.header("Ahorcado")
-#st.audio("media/suspense.mp3", loop=True, autoplay=True)
 
 state = st.session_state
 if "answer" not in state or state.answer == "":
     # Get all Spanish words
-    spanish_words = [word.lower() for sentence in cess_esp.sents()[:100] for word in sentence if len(word) >= 5 and word.isalpha()][:100]
+    spanish_words = [word.lower() for sentence in cess_esp.sents()[:200] for word in sentence if len(word) >= 5 and word.isalpha()]
 
-    # st.write(f"Number of Spanish words: {len(spanish_words)}")
-
-    state["answer"] = "CAÑO"  # random.choice(spanish_words).upper()
+    state["answer"] = random.choice(spanish_words).upper()
     state["known_letters"] = set()
-    state.known_letters.add(unidecode(state.answer[0]))
-    state.known_letters.add(unidecode(state.answer[-1]))
+    state.known_letters.add(remove_accents(state.answer[0]))
+    state.known_letters.add(remove_accents(state.answer[-1]))
     state["attempted_letters"] = set()
     state["finished"] = False
 
 c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26 = st.columns(27)
 with c0:
-    # if st.button("A", use_container_width=True):
     button("A")
 with c1:
     button("B")
@@ -65,8 +67,8 @@ with c12:
     button("M")
 with c13:
     button("N")
-# with c14:
-#     button("Ñ")
+with c14:
+    button("Ñ")
 with c15:
     button("O")
 with c16:
@@ -93,12 +95,12 @@ with c26:
     button("Z")
 
 if state.attempted_letters:
-    st.write("Intentadas: " + " ".join(sorted(state.attempted_letters, key=lambda x: unidecode(x))))
+    st.write("Intentadas: " + " ".join(sorted(state.attempted_letters, key=lambda x: remove_accents(x))))
 
 shown_word = ""
 all_known = True
 for i in range(len(state.answer)):
-    if unidecode(state.answer[i]) in state.known_letters:
+    if remove_accents(state.answer[i]) in state.known_letters:
         shown_word += state.answer[i]
     else:
         shown_word += '_'
@@ -107,16 +109,22 @@ for i in range(len(state.answer)):
 st.subheader(shown_word)
 
 if all_known:
-    st.header("¡Ganaste!")
+    st.header("¡Acertaste!")
+    st.audio("media/win.mp3", loop=False, autoplay=True)
     state.finished = True
 
 attempts = len(state.attempted_letters)
-if attempts > 0:
-    st.image(f"media/hanged_{attempts}.png")
 
 if attempts == 6:
     st.header(f"¡Perdiste! La palabra era {state.answer}")
+    st.audio("media/lose.mp3", loop=False, autoplay=True)
     state.finished = True
+
+if attempts > 0:
+    with st.sidebar:
+        st.image(f"media/hanged_{min(6, attempts)}.png")
+        if not state.finished:
+            st.audio("media/suspense_low_vol.mp3", loop=True, autoplay=True)
 
 if state.finished and st.button("Volver a jugar"):
     state.answer = ""
